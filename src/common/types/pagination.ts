a95c358cff59ce9147ccb5b9180357e0dc311de5
grasshopper42 <solite4686@gmail.com>
2024-12-05 23:50:07 +0900

    featur: Pagination

---
+++ src/common/types/pagination.ts
@@ -0,0 +1,17 @@
+export interface Pagination {
+  page: number;
+  limit: number;
+  total: number;
+}
+
+export interface PaginatedResponse<T> {
+  items: T[];
+  pagination: {
+    page: number;
+    limit: number;
+    total: number;
+    totalPages: number;
+    hasPrev: boolean;
+    hasNext: boolean;
+  };
+}
